f03fb285d4068e142a13d1a930d9ef6179dd402c
Krishna K Maddula <kk17july@gmail.com>
2018-07-03 06:30:59 -0700

    Added mocha & beforeEach to retrieve accounts from ganache

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,19 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+
+describe('Inbox test', () => {
+
+    beforeEach(() => {
+       web3.eth.getAccounts()
+            .then(fetchedAccounts => { 
+                console.log(fetchedAccounts);
+            });
+    });
+
+    it('', () => {
+
+    });
+});
