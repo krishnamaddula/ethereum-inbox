5f862528a1c0ecbd809ef46b9719987d78ab08c6
Krishna K Maddula <kk17july@gmail.com>
2018-07-03 18:21:59 -0700

    test deployment and default initial message

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -18,6 +18,13 @@ describe('Inbox test', () => {
     });
 
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal('Hi there!',message);
+    });
+
+
 });
