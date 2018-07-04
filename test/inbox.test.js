4b9d8106295ba8b4a0c07ccce7ef188834d56bb0
Krishna K Maddula <kk17july@gmail.com>
2018-07-03 17:58:46 -0700

    Deploy web3 contract using testcase

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,18 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile')
 
+let accounts;
+let inbox;
 
 describe('Inbox test', () => {
 
-    beforeEach(() => {
-       web3.eth.getAccounts()
-            .then(fetchedAccounts => { 
-                console.log(fetchedAccounts);
-            });
+    beforeEach(async () => {
+       accounts = await web3.eth.getAccounts();
+
+       inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .send({from: accounts[0], gas: '1000000'})
     });
 
-    it('', () => {
-
+    it('deploys a contract', () => {
+        console.log(inbox);
     });
 });
